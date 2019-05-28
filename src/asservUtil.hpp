cf4474041926b758c0dc6ee6d7b7c1f02f888404
ShinySilver <nicolas.louismarie@gmail.com>
2019-05-28 19:51:10 +0200

    Added debugging

--- src/asservUtil.hpp
+++ src/asservUtil.hpp
@@ -18,12 +18,16 @@ namespace AsservUtil{
     static inline void forward(int distance){
         if(motionBase){
             motionBase->sendCommand("forward:"+std::to_string(distance)+";");
+        }else{
+            std::cout<<"Une commande a été envoyée à la MotionBase alors que la lib n'était pas initialisée\n";
         }
     }
 
     static inline void rotate(double angle){
         if(motionBase){
             motionBase->sendCommand("rotate:"+std::to_string(angle)+";");
+        }else{
+            std::cout<<"Une commande a été envoyée à la MotionBase alors que la lib n'était pas initialisée\n";
         }
     }
 
@@ -35,6 +39,8 @@ namespace AsservUtil{
             }
             command+=std::to_string(targetedAngle);
             motionBase->sendCommand(command+";");
+        }else{
+            std::cout<<"Une commande a été envoyée à la MotionBase alors que la lib n'était pas initialisée\n";
         }
     }
 

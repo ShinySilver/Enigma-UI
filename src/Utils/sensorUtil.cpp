ea330ef516ee1e4666b9395788a6c1e01fb8d2aa
ShinySilver <nicolas.louismarie@gmail.com>
2019-05-29 17:30:30 +0200

    ~

--- src/Utils/sensorUtil.cpp
+++ src/Utils/sensorUtil.cpp
@@ -6,7 +6,9 @@ void Utils::SensorUtil::cb(const std::string& str){
     std::cout<<"sensor #"<<str[0]-'0'<<" passe à "<<str[2]-'0'<<"\n";
     if(str[0]-'0'>= 0 && str[0]-'0'<SENSOR_COUNT){
         instance()->sensorMutex.lock();
-        if(instance_->enabledSensors[str[0]-'0']){
+        std::cout<<"GNE\n";
+        std::cout<<instance()<<","<<instance_<<'\n';
+        if(instance_->enabledSensors[str[0]-'0']==true){
             std::cout<<"Le capteur #"<<str[0]<<" est passé à l'état "<<str[2]<<'\n';
             instance_->sensorValues[str[0]-'0']=(str[2]-'0'!=0);
             if(str[2]-'0'==1){
